7778d5c2ada6bd6111308efbc4c8e0f0c6825c2c
Rijan Gautam <rijangautam07@gmail.com>
2025-04-25 18:15:30 +0545

    update (the last update)
    This is the last update before I pause this project. I am still learning django and some component of this project is out of my level. So I am resuming my learning and after that I will continue the project. :)

--- base/static/js/script.js
+++ base/static/js/script.js
@@ -1,7 +1,6 @@
 const button = document.getElementById("addnote_button");
 const box = document.getElementById("addnote_background");
-const editNote_button = document.getElementById("note_link");
-const editNote_box = document.getElementById("editNote")
+
 
 
 button.addEventListener("click", () => {
@@ -13,7 +12,13 @@ button.addEventListener("click", () => {
       }  
 });
 
-button.addEventListener("click", () => {
-  
+function toggleBox() { 
+  const box = document.getElementById('viewnote_background');
 
-});
+  if (box.style.display === "flex") {
+    box.style.display = "none";  // Hide the box if it's visible
+  } else {
+    box.style.display = "flex";  // Show the box if it's hidden
+  }  
+
+}
