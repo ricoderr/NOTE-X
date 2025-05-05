aff353bc6eb3dedb869a55a28ef3cf0f0c2c2daf
Rijan Gautam <rijangautam07@gmail.com>
2025-05-05 14:54:56 +0545

    done

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Notes, Sticky_notes
 
+admin.site.site_header = "NOTE-X Admin panel"
+admin.site.index_title = "Admin"
 admin.site.register(Notes)
 admin.site.register(Sticky_notes)
--- base/static/js/script.js
+++ base/static/js/script.js
@@ -12,13 +12,3 @@ button.addEventListener("click", () => {
       }  
 });
 
-function toggleBox() { 
-  const box = document.getElementById('viewnote_background');
-
-  if (box.style.display === "flex") {
-    box.style.display = "none";  // Hide the box if it's visible
-  } else {
-    box.style.display = "flex";  // Show the box if it's hidden
-  }  
-
-}
