8a4e421b94cf4bc6dcc6e2bdae0697d8482e5190
Rijan Gautam <rijangautam07@gmail.com>
2025-08-30 23:33:01 +0545

    Ready for deployment

--- notebook/settings.py
+++ notebook/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q_^c)g+rinsyk)osl@4lfr321@uq-aq7yd&a==jnaz+$wp5&g0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
